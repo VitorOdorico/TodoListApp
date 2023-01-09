1abb56055c1c83da6cd1bb269af50dc80c39f93a
VitorOdorico <ve743137@gmail.com>
2023-01-09 20:32:47 -0300

    resolvendo bug na função render

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -3,19 +3,30 @@ let todoInput = document.querySelector('.todo-input');
 const completedTodosDivs = document.querySelector('.completed-todos');
 const uncompletedTodosDivs = document.querySelector('.uncompleted-todos');
 
-window.onload = () => {};
+window.onload = () => {
+    let storageTodoItems = localStorage.getItem('todoItems')
+    if(storageTodoItems !== null){
+        todoItems = JSON.parse(storageTodoItems)
+    }
+    render()
+};
 
-todoInput.onkeyup((e) =>{
+todoInput.onkeyup = ((e) =>{
     let value = e.target.value.replace(/^\s+/, "")
     if(value && e.keyCode === 13){
         addTodo(value)
         todoInput.value = ''
         todoInput.focus()
-    }
+    };
 })
 
 function addTodo(text){
-
+    todoItems.push({
+        id: Date.now(),
+        text,
+        completed: false
+    });
+    saveAsRender();
 }
 
 function removeTodo(id){
@@ -34,13 +45,13 @@ function save(){
 
 }
 
-function reder(){
-    
+function render(){
+
 }
 
 function saveAsRender(){
     save()
-    reder()
+    render()
 }
 
 function createTodoElement(todo){
