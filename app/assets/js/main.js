5855ef4a01d72087a80219db3fca362903860bcb
VitorOdorico <ve743137@gmail.com>
2023-01-09 22:00:50 -0300

    add to render()

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -42,11 +42,31 @@ function markAsUncompleted(id){
 }
 
 function save(){
+    localStorage.setItem('todoItems', JSON.stringify(todoItems))
 
 }
 
 function render(){
+    let unCompletedTodos = todoItems.filter(item => !item.completed)
+    let completedTodos = todoItems.filter(item => item.completed)
 
+    completedTodosDivs.innerHTML=''
+    uncompletedTodosDivs.innerHTML=''
+
+    if(unCompletedTodos.length > 0){
+        unCompletedTodos.forEach(todo => {
+            uncompletedTodosDivs.append(createTodoElement(todo))
+        })
+    }else{
+        uncompletedTodosDivs.innerHTML = `<div class='empty'>No uncompleted mission </div>`
+    }
+
+    if(completedTodos.length > 0){
+        completedTodosDivs.innerHTML = `<div class='completed-title'>Completed (${completedTodos.length} / ${todoItems.length}) </div>`
+        completedTodosDivs.forEach(todo =>{
+            completedTodosDivs.append(createTodoElement(todo))
+        })
+    }
 }
 
 function saveAsRender(){
